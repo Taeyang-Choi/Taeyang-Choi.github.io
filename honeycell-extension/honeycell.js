cebfccf10acaa1bf712757310d52667a39544510
Taeyang-Choi <zhfldk1027@gmail.com>
2017-11-15 12:08:46 +0900

    honeycell test version

--- honeycell-extension/honeycell.js
+++ honeycell-extension/honeycell.js
@@ -1,16 +1,54 @@
 (function(ext) {
-    // Cleanup function when the extension is unloaded
+
+    var connected = false;
+    var device = null; // change to honeycell
+    var rawData = null;
+
     ext._shutdown = function() {};
 
-    // Status reporting code
-    // Use this to report missing hardware, plugin or unsupported browser
     ext._getStatus = function() {
-        return {status: 2, msg: 'Ready'};
+        if (!connected)
+            return { status:1, msg:'Disconnected' };
+        else
+            return { status:2, msg:'Connected' };
     };
 
-    // Functions for block with type 'w' will get a callback function as the 
-    // final argument. This should be called to indicate that the block can
-    // stop waiting.
+    ext._deviceRemoved = function(dev) {
+        console.log('Device removed');
+    };
+
+    var comport = [];
+    ext._deviceConnected = function(dev) {
+        comport.push(dev);
+        if (!device) tryNextDevice();
+    };
+
+    var poller = null;
+    var watchdog = null;
+    function tryNextDevice() {
+        device = comport.shift();
+        if(!device) return;
+
+        device.open({ stopBits: 0, bitRate: 57600, ctsFlowControl: 0 });
+        console.log('Attempting connection with ' + device.id);
+        device.set_receive_handler(function(data) {
+            rawData = new Uint8Array(data);
+            console.log(rawData);
+        });
+
+        watchdog = setTimeout(function() {
+            device.set_receive_handler(null);
+            device.close();
+            device = null;
+            tryNextDevice();
+        }, 5000);
+    }
+
+    ext._shutdown = function() {
+        if (device) device.close();
+        device = null;
+    };
+
     ext.wait_random = function(callback) {
         wait = Math.random();
         console.log('Waiting for ' + wait + ' seconds');
@@ -19,13 +57,11 @@
         }, wait*1000);
     };
 
-    // Block and block menu descriptions
     var descriptor = {
         blocks: [
             ['w', 'wait for random time', 'wait_random'],
         ]
     };
-
-    // Register the extension
-    ScratchExtensions.register('Random wait extension', descriptor, ext);
+    
+    ScratchExtensions.register('HoneyCell', descriptor, ext, {type:'serial'});
 })({});
