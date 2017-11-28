c9ebcb262823721ef6953345eb8bc13d211dcd2d
Taeyang-Choi <zhfldk1027@gmail.com>
2017-11-28 13:04:43 +0900

    171128-09

--- honeycell-extension/honeycell.js
+++ honeycell-extension/honeycell.js
@@ -361,7 +361,7 @@
     var potentialDevices = [];
     ext._deviceConnected = function(dev) {
         potentialDevices.push(dev);
-        
+        console.log(potentialDevices);
         if (!device) { 
             console.log("Try Connect!!");
             tryNextDevice();
